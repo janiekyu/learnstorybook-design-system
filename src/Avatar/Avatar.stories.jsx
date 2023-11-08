7a7865f44e809abf27d93661c2d1b14892875e2c
Jane <jhur@us.ci.org>
2023-11-08 11:51:36 -0600

    Add Storybook

--- src/Avatar/Avatar.stories.jsx
+++ src/Avatar/Avatar.stories.jsx
@@ -66,3 +66,12 @@ export const Large = {
     </>
   ),
 };
+
+export const Controls = {
+  args: {
+    loading: false,
+    size: 'tiny',
+    username: 'Tom Cruise',
+    src: 'https://avatars.githubusercontent.com/u/263385',
+  },
+};
